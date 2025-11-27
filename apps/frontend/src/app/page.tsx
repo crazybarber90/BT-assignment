bb011baebe299186b74aee409e74a0624ddf7e8f
Nikola <pepy90aa@gmail.com>
2025-11-27 13:13:13 +0100

    refactored to accept csv-file (input-file instead text-aread)

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -27,6 +27,20 @@ export default function HomePage() {
     uploadMutation.mutate({ csvText: csv })
   }
 
+  const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if (!file) return
+
+    const reader = new FileReader()
+
+    reader.onload = (event) => {
+      const text = event.target?.result as string
+      setCsv(text) // ubacujemo CSV u state koji backend već očekuje
+    }
+
+    reader.readAsText(file)
+  }
+
   if (isLoading) {
     return (
       <div className="fixed inset-0 flex items-center justify-center bg-gray-900 z-50">
@@ -46,29 +60,43 @@ export default function HomePage() {
       <h1 className="text-3xl font-extrabold text-(--primary)">Member List</h1>
 
       {/* CSV Upload */}
-      <div className="space-y-3">
-        <textarea
+      <div className="space-y-3 flex flex-col">
+        {/* <textarea
           className="w-full p-4 rounded-lg border-2 border-gray-700 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-(--primary)"
           rows={6}
           value={csv}
           onChange={(e) => setCsv(e.target.value)}
           placeholder="Paste CSV here..."
+        /> */}
+
+        <input
+          type="file"
+          accept=".csv"
+          onChange={handleFile}
+          className="w-60 h-16 p-3 rounded-lg bg-gray-800 border border-gray-700"
         />
-        <button
+        {/* <button
           onClick={handleUpload}
           className="w-full py-3 rounded-lg bg-linear-to-r from-(--primary) to-(--secondary) text-white font-semibold shadow-md hover:scale-101 hover:from-(--primary) hover:to(--secondary) active:scale-95 transform transition cursor-pointer"
+        >
+          Upload CSV
+        </button> */}
+
+        <button
+          onClick={handleUpload}
+          className="w-60 h-20 flex items-center justify-center py-3 rounded-lg bg-linear-to-r from-(--primary) to-(--secondary) text-white font-semibold shadow-md hover:scale-101 active:scale-95 transform transition cursor-pointer"
         >
           Upload CSV
         </button>
       </div>
 
       {/* Member List */}
-      <div className="space-y-4">
+      <div className="space-y-4 flex w-full justify-around gap-4">
         {members?.map((m: any) => (
           <Link
             href={`/members/${m.id}`}
             key={m.id}
-            className="block p-4 rounded-lg bg-gray-800 hover:bg-gray-700 shadow-md transition"
+            className="flex-1 h-36 p-4 rounded-lg bg-gray-800 hover:bg-gray-700 shadow-md transition"
           >
             <h2 className="text-(--primary) font-bold text-lg">
               {m.firstName} {m.lastName}
